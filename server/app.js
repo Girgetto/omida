ee5c5924ee5ce2b17453398d814ac48b214ce3ac
Girgetto <giorgiograssini@gmail.com>
2019-10-17 17:16:23 +0200

    Init server

---
+++ server/app.js
@@ -0,0 +1,15 @@
+  
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const passport = require('passport');
+const flash = require('connect-flash');
+const debug = require('debug')('app');
