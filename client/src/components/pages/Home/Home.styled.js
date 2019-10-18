07e8fb75d4ebddcf191598f14be823edd796d65c
Girgetto <giorgiograssini@gmail.com>
2019-10-18 19:01:03 +0200

    Styling

--- client/src/components/pages/Home/Home.styled.js
+++ client/src/components/pages/Home/Home.styled.js
@@ -1,4 +1,10 @@
 import styled from 'styled-components';
 import Home from './Home.component';
 
-export default styled(Home).attrs({})``;
+export default styled(Home).attrs({})`
+  height: 924px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
