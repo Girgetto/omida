bfba001697263799e264ef9edd4fb6c3d73b6ceb
Giorgio Grassini <giorgiograssini@gmail.com>
2019-11-16 19:23:31 +0100

    Removed fixed

--- client/src/components/commons/Navbar/Navbar.styled.js
+++ client/src/components/commons/Navbar/Navbar.styled.js
@@ -5,17 +5,17 @@ export default styled(Navbar).attrs({})`
   height: 40px;
   display: flex;
   align-items: center;
-  position: fixed;
   right: 0;
   left: 0;
-
+  background-color: #fff;
+  
   .list {
     width: 200px;
     display: flex;
     justify-content: space-around;
     
     a {
-      color: #fff;
+      color: #4b4d4f;
       text-decoration: none;
     }
   }
