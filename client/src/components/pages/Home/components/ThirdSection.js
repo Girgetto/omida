1e95c5c713cb47c1dd3f773b28f70f8a6edd9f2e
Giorgio Grassini <giorgiograssini@gmail.com>
2020-01-11 12:25:54 +0100

    Changed font

--- client/src/components/pages/Home/components/ThirdSection.js
+++ client/src/components/pages/Home/components/ThirdSection.js
@@ -3,8 +3,16 @@ import React from 'react';
 const flexCenter = {
   display: 'flex',
   justifyContent: 'center',
-  alignItems: 'center',
-}
+  alignItems: 'center'
+};
+
+const title = {
+  fontSize: '24px'
+};
+
+const paraph = {
+  color: 'rgba(255, 255, 255, 0.6)'
+};
 
 const ThirdSection = () => {
   return (
@@ -14,27 +22,27 @@ const ThirdSection = () => {
         height: '200px'
       }}
     >
-      <div style={{ textAlign: 'center'}}>
-        <h1>About</h1>
-        <p>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={title}>About</h1>
+        <p style={paraph}>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae odio
           magnam, porro esse necessitatibus saepe asperiores totam veritatis
           quia ullam ducimus veniam quo in. Vel, ratione? Vel repudiandae minus
           aliquid!
         </p>
       </div>
-      <div style={{ textAlign: 'center'}}>
-        <h1>Service</h1>
-        <p>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={title}>Service</h1>
+        <p style={paraph}>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae odio
           magnam, porro esse necessitatibus saepe asperiores totam veritatis
           quia ullam ducimus veniam quo in. Vel, ratione? Vel repudiandae minus
           aliquid!
         </p>
       </div>
-      <div style={{ textAlign: 'center'}}>
-        <h1>Ipsum</h1>
-        <p>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={title}>Ipsum</h1>
+        <p style={paraph}>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae odio
           magnam, porro esse necessitatibus saepe asperiores totam veritatis
           quia ullam ducimus veniam quo in. Vel, ratione? Vel repudiandae minus
