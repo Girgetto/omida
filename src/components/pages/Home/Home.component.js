bcd6a933d78c71741e86be13a236f69b612eb2b4
Giorgio Grassini <giorgiograssini@gmail.com>
2020-02-23 09:24:51 +0100

    Before eject

--- src/components/pages/Home/Home.component.js
+++ src/components/pages/Home/Home.component.js
@@ -1,14 +1,14 @@
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
 import * as Comp from './components';
 
 const Home = ({ className }) => {
   return (
-  <Suspense fallback={<div>Loading</div>}>
-    <div className={className}>
-      <Comp.FirstSection/>
-      <Comp.SecondSection/> 
-      <Comp.ThirdSection/> 
-    </div>
+    <Suspense fallback={<div>Loading</div>}>
+      <div className={className}>
+        <Comp.FirstSection />
+        <Comp.SecondSection />
+        <Comp.ThirdSection />
+      </div>
     </Suspense>
   );
 };
--- src/components/pages/Home/components/SecondSection.js
+++ src/components/pages/Home/components/SecondSection.js
@@ -1,15 +1,9 @@
 import React from 'react';
+import style from './style.scss';
 
 const SecondSection = () => {
   return (
-    <section
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '200px'
-      }}
-    >
+    <section className={style.secondSection}>
       <p>
         We are an elite security firm specializing in identifying risk and
         developing customized solutions for high profile clients.
