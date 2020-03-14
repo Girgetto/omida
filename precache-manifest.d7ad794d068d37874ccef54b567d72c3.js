53c28ba56033706050c17b3e673490407147bbea
Giorgio Grassini <giorgiograssini@gmail.com>
2020-03-14 21:56:00 +0100

    Updates

---
+++ precache-manifest.d7ad794d068d37874ccef54b567d72c3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed9850b7dd87e53411ab58b2458a16e",
+    "url": "/DoubleOSeven/index.html"
+  },
+  {
+    "revision": "03cf61420cd1782946d2",
+    "url": "/DoubleOSeven/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "32472c24bae387442b8b",
+    "url": "/DoubleOSeven/static/css/main.ce5e658a.chunk.css"
+  },
+  {
+    "revision": "03cf61420cd1782946d2",
+    "url": "/DoubleOSeven/static/js/2.1d3ef996.chunk.js"
+  },
+  {
+    "revision": "32472c24bae387442b8b",
+    "url": "/DoubleOSeven/static/js/main.ae1edb28.chunk.js"
+  },
+  {
+    "revision": "3276fa0747f723a5538c",
+    "url": "/DoubleOSeven/static/js/runtime-main.069ef8bb.js"
+  },
+  {
+    "revision": "4fb59ba11bdbe7adda091a7ae6015990",
+    "url": "/DoubleOSeven/static/media/castle.4fb59ba1.jpg"
+  },
+  {
+    "revision": "3f8db8327506d4e7393d755ceff1c47b",
+    "url": "/DoubleOSeven/static/media/view1.3f8db832.jpg"
+  },
+  {
+    "revision": "2665f2b42cffab4601f93468250e04cf",
+    "url": "/DoubleOSeven/static/media/view2.2665f2b4.jpg"
+  },
+  {
+    "revision": "d799d264fee3764549d718baebbfdf5f",
+    "url": "/DoubleOSeven/static/media/view3.d799d264.jpg"
+  }
+]);
