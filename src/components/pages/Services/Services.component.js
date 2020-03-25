0955bec5c1b11a4d0f9077c4c3fe0e51b0e91ecb
Giorgio Grassini <giorgiograssini@gmail.com>
2020-03-25 20:37:49 +0100

    Added services translation

--- src/components/pages/Services/Services.component.js
+++ src/components/pages/Services/Services.component.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const Services = () => {
+  const [t] = useTranslation();
   return (
     <div>
-      Services
+      {t('services.Intro')}
+      {t('services.IntroText')}
+      {t('services.LuxuryRealEstate')}
+      {t('services.BusinessAdvisory')}
+      {t('services.TechnicalAdvice')}
+      {t('services.Refurbishment')}
+      {t('services.BusinessMaximization')}
+      {t('services.Management')}
+      {t('services.Coordination')}
+      {t('services.AfterSales')}
+      {t('services.Planning')}
+      {t('services.TheTarget')}
+      {t('services.ADeep')}
+      {t('services.WeWill')}
+      {t('services.WeCoordinate')}
     </div>
   )
 }
