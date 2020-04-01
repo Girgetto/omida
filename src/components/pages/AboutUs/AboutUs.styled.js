4fdb6e68de59748141f88b09945cff22b2def075
Giorgio Grassini <giorgiograssini@gmail.com>
2020-04-01 20:38:16 +0200

    Update service titles

--- src/components/pages/AboutUs/AboutUs.styled.js
+++ src/components/pages/AboutUs/AboutUs.styled.js
@@ -4,7 +4,7 @@ import AboutUs from './AboutUs.component';
 export default styled(AboutUs).attrs({})`
   margin: 0 auto;
   width: 60%; 
-  height: 80vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
--- src/components/pages/Services/Services.component.js
+++ src/components/pages/Services/Services.component.js
@@ -8,15 +8,13 @@ const Services = ({ className }) => {
       <h1>{t('services.Intro')}</h1>
       <p>{t('services.IntroText')}</p>
       <div className="titles">
-        <h1>{t('services.LuxuryRealEstate')}</h1>
-        <h1>{t('services.BusinessAdvisory')}</h1>
-        <h1>{t('services.TechnicalAdvice')}</h1>
-        <h1>{t('services.Refurbishment')}</h1>
-        <h1>{t('services.BusinessMaximization')}</h1>
-        <h1>{t('services.Management')}</h1>
-        <h1>{t('services.Coordination')}</h1>
-        <h1>{t('services.AfterSales')}</h1>
-        <h1>{t('services.Planning')}</h1>
+        <h2>
+          {t('services.LuxuryRealEstate')} | {t('services.BusinessAdvisory')} | 
+          {t('services.TechnicalAdvice')} | {t('services.Refurbishment')} | 
+          {t('services.BusinessMaximization')} | {t('services.Management')} | 
+          {t('services.Coordination')} | {t('services.AfterSales')} | 
+          {t('services.Planning')}
+        </h2>
       </div>
       <p>{t('services.TheTarget')}</p>
       <p>{t('services.ADeep')}</p>
