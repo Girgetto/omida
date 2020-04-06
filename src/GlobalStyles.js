ce35bc4889a99c5307300cad38c5a0a2093f6d03
Giorgio Grassini <giorgiograssini@gmail.com>
2020-04-06 19:57:57 +0200

    Refactor GlobalStyle

---
+++ src/GlobalStyles.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Notable');
+  body {
+    font-family: 'Poppins', sans-serif;
+    vertical-align: baseline;
+    color:#fff;
+    background-color: #000;
+  }
+
+  h1 {
+    font-size: 3rem;
+    font-weight: 100;
+    line-height: 4rem;
+  }
+
+  p {
+    font-weight: 100;
+    color: rgba(255,255,255,0.6);
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -10,27 +10,8 @@ import Routes from './routes';
 import './style.scss';
 import './i18n';
 import * as serviceWorker from './serviceWorker';
-import { createGlobalStyle } from 'styled-components';
-const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Notable');
-  body {
-    font-family: 'Poppins', sans-serif;
-    vertical-align: baseline;
-    color:#fff;
-    background-color: #000;
-  }
+import GlobalStyles from './GlobalStyles';
 
-  h1 {
-    font-size: 3rem;
-    font-weight: 100;
-    line-height: 4rem;
-  }
-
-  p {
-    font-weight: 100;
-    color: rgba(255,255,255,0.6);
-  }
-`;
 AOS.init();
 
 ReactDOM.render(
