fcc9e1fe5ccc258241952b5970d14e1879cbe739
Giorgio Grassini <giorgiograssini@gmail.com>
2020-04-11 15:47:41 +0200

    Feature(): Added foto

--- src/components/pages/Home/Home.styled.js
+++ src/components/pages/Home/Home.styled.js
@@ -1,19 +1,4 @@
 import styled from 'styled-components';
 import Home from './Home.component';
 
-export default styled(Home).attrs({})`
-  .firstSection {
-    height: 724px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    background-color: black;
-
-    .mainTitle {
-      font-size: 46px;
-      font-weight: 500;
-      color: white;
-    }
-  }
-`;
+export default styled(Home).attrs({})``;
--- src/components/pages/Home/components/Main.js
+++ src/components/pages/Home/components/Main.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import style from './style.module.scss';
 
 const Main = () => {
   const [t] = useTranslation();
   return (
-  <section className="firstSection">
-    <h1 className="mainTitle">{t('title')}</h1>
-    <p>{t('home.RealEstate')}</p>
-  </section>
-)};
+    <section className={style.firstSection}>
+      <div className={style.shadow}>
+        <img src={require('../../../../assets/img/franco.jpg')} alt="franco" />
+      </div>
+      <div className={style.mainTitle}>
+        <h1>{t('title')}</h1>
+        <p>{t('home.RealEstate')}</p>
+      </div>
+    </section>
+  );
+};
 
 export default Main;
