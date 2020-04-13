6ec214eb069933f3a17e8b04584d81ae62979ec5
Giorgio Grassini <giorgiograssini@gmail.com>
2020-04-13 20:56:35 +0200

    Hotfix(reducer): user error

--- src/state/index-reducers.js
+++ src/state/index-reducers.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux'
 import ui from './UI/reducer'
-import user from './User/reducer'
 
-const AppReducer = combineReducers({ ui, user })
+const AppReducer = combineReducers({ ui })
 
 const rootReducer = (state, action) => {
   if (action.type === 'LOGOUT') return AppReducer({}, action)
