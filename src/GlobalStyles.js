878a17144bdfb83f0ffa13dd2a93221a8ec3317d
Giorgio Grassini <giorgiograssini@gmail.com>
2020-04-21 12:42:22 +0200

    Favicon

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,9 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-  @import url('/assets/Font/Avenir Next West/AvenirNext-UltraLight.ttf');
-  @import url('/assets/Font/Avenir Next West/AvenirNext-Bold.ttf');
-  @import url('/assets/Font/Avenir Next West/AvenirNext-Regular.ttf');
+  @import url('/assets/font/Avenir-Next-West/AvenirNext-UltraLight.ttf');
+  @import url('/assets/font/Avenir-Next-West/AvenirNext-Bold.ttf');
+  @import url('/assets/font/Avenir-Next-West/AvenirNext-Regular.ttf');
+
   body {
     font-family: 'AvenirNext-UltraLight';
     vertical-align: baseline;
@@ -20,6 +21,7 @@ export default createGlobalStyle`
     line-height: 6rem;
     letter-spacing: 25px;
   }
+
   h2 {
     font-family: AvenirNext-Regular;
     font-size: 24px;
@@ -27,6 +29,7 @@ export default createGlobalStyle`
     line-height: 25px;
     letter-spacing: 3px;
   }
+
   h3 {
     font-family: AvenirNext-Regular;
     font-size: 20px;
@@ -38,18 +41,21 @@ export default createGlobalStyle`
     font-weight: 100;
     color: rgba(255,255,255,0.6);
   }
+
   span {
     font-family: AvenirNext-Regular;
     font-size: 14px;
     font-weight: 100;
     line-height: 2,5rem;
   }
+
   ul {
     font-family: AvenirNext-Bold;
     font-size: 40px;
     font-weight: 100;
     line-height: 2,5rem;
   }
+
   a {
     font-family: AvenirNext-Regular;
     font-size: 20px;
@@ -57,4 +63,4 @@ export default createGlobalStyle`
     line-height: 40px;
     letter-spacing: 2px;
   }
-`;
+`
