9b9e0d1ed23d3b0c63de23d4ff61d7a3dff1bb64
Giorgio Grassini <giorgiograssini@gmail.com>
2020-04-23 11:21:45 +0200

    Remoda errors in translation

--- src/i18n.js
+++ src/i18n.js
@@ -18,8 +18,6 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    defaultNS: 'general',
-    ns: ['general', 'errors', 'warnings'],
   })
 
 export default i18n
